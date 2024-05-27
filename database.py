274a62498d29e0f18fe4588c189d20cc96d338ad
prøveeksamen-emil <emirim01@innlandetfylke.no>
2024-05-27 12:13:09 +0200

    endret databasen slik at den også henter inn bildene fra static/barcode basert på hvilket boknummer som har lik tall som bildet

--- database.py
+++ database.py
@@ -10,14 +10,23 @@ CREATE TABLE IF NOT EXISTS Books (
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     isbn INTEGER,
-    booknumber INTEGER
+    booknumber INTEGER,
+    image_path TEXT
 )''')
 
 with open('bøker.csv', 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
-    books = [(row['Tittel'], row['Forfatter'], int(row['ISBN']), int(row['Strekkode'])) for row in reader]
+    books = []
+    for row in reader:
+        title = row['Tittel']
+        author = row['Forfatter']
+        isbn = int(row['ISBN'])
+        booknumber = int(row['Strekkode'])
+        image_path = f'static/barcode/{booknumber}.png'  
+        books.append((title, author, isbn, booknumber, image_path))
 
-cursor.executemany('INSERT INTO Books(title, author, isbn, booknumber) VALUES (?, ?, ?, ?)', books)
+
+cursor.executemany('INSERT INTO Books(title, author, isbn, booknumber, image_path) VALUES (?, ?, ?, ?, ?)', books)
 
 conn.commit()
 conn.close()
